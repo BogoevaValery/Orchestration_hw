1d42da29303df2e6c3da456d75842ff7856c144c
BogoevaValery <bogoeva.v@icloud.com>
2026-03-17 16:19:08 +0300

    add logging in compl

--- hw1/complement.py
+++ hw1/complement.py
@@ -3,8 +3,12 @@
 from Bio.Seq import Seq
 from Bio.SeqUtils import gc_fraction 
 import argparse
+import logging
 
-qwazexrydctufvyigubhinjmkp,l.;xrdtcfyvgubinom
+logging.basicConfig(
+    level=logging.INFO, 
+    filename="complement.log",
+    filemode="w")
 
 parser = argparse.ArgumentParser(description="My script to get reverse complement dna strand and gc fraction")
 parser.add_argument("--seq", type=str, help="dna string to process")
@@ -12,8 +16,13 @@ args = parser.parse_args()
 seq = args.seq
 
 seq_bp = Seq(seq)
+logging.info(f"Running complement.py on {seq_bp}")
+
 rev_comp = seq_bp.reverse_complement()
+logging.info(f"Reverse complement: {rev_comp}")
+
 gc = gc_fraction(seq_bp)
+logging.info(f"GC-fraction: {gc:.3f}")
 
 print(rev_comp)
 print(f"{gc:.3f}")

e0df439c410b196d89b9ba77059657a4f0f44b15
BogoevaValery <bogoeva.v@icloud.com>
2026-03-17 03:04:58 +0300

    broken complement

--- hw1/complement.py
+++ hw1/complement.py
@@ -4,6 +4,10 @@ from Bio.Seq import Seq
 from Bio.SeqUtils import gc_fraction 
 import argparse
 
+йцукенгджлосчвесанмпорлдэмдгслнычас пмиотльлдодтлоесгми\=
+зэоснпмдриотльб
+нгитьэ
+
 parser = argparse.ArgumentParser(description="My script to get reverse complement dna strand and gc fraction")
 parser.add_argument("--seq", type=str, help="dna string to process")
 args = parser.parse_args()
